a169fe8c7faae882865bf76ed9c2da1bab9a12e0
Abhijeet Kumar <110721083+Abhijeet966@users.noreply.github.com>
2023-11-09 17:57:00 +0530

    update

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 let imgName = "./assets/Toggle";
-let image =  document.querySelector(".hero_img");
-let upArrow = document.querySelector("#upArrow")
-let downArrow = document.querySelector("#downArrow")
+let image = document.querySelector(".hero_img");
+let upArrow = document.querySelector("#upArrow");
+let downArrow = document.querySelector("#downArrow");
 let count = document.querySelector(".count1");
 let filter = document.querySelector(".filters");
 
-function imageToggeler(){
-    let id = 0;
-     function increment(){
-        id = ++id%5; 
-        console.log(imgName + id + ".jpg");
-        image.src = imgName + id + ".jpg";
-        count.textContent = String(id + 1).padStart(2,0);
-        console.log(String(id + 1).padStart(2,0));
-    }    
-     function decrement(){
-        if(id<=0){
-            id = 4;
-        }
-        id--;
-        image.src = imgName + id +".jpg";
-        count.textContent = String(id + 1).padStart(2,0);
-    }   
-    return {increment,decrement}; 
+function imageToggeler() {
+  let id = 1;
+  function increment() {
+    if (id == 5) {
+        id = 1;
+      }
+    id = id % 5;
+    console.log(imgName + id + ".jpg");
+    image.src = imgName + id + ".jpg";
+    // count.textContent = String(id + 1).padStart(2,0);
+    // console.log(String(id + 1).padStart(2,0));
+    id++;
+  }
+  function decrement() {
+    if (id <= 0) {
+      id = 4;
+    }
+    id--;
+    image.src = imgName + id + ".jpg";
+    count.textContent = String(id + 1).padStart(2, 0);
+  }
+  return { increment, decrement };
 }
 
-const {increment,decrement} = imageToggeler();
+const { increment, decrement } = imageToggeler();
 
-upArrow.addEventListener("click",increment);
-downArrow.addEventListener("click",decrement);
+upArrow.addEventListener("click", increment);
+downArrow.addEventListener("click", decrement);
 
-filter.addEventListener("click",(e)=>{
-    e.stopPropagation();
-    if ( e.target && e.target.className!=="filters" && e.target.matches('div')) {
-        document.querySelector(".bold").classList.remove("bold");
-        e.target.className = "bold";
-    }
-})
+filter.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target && e.target.className !== "filters" && e.target.matches("div")) {
+    document.querySelector(".bold").classList.remove("bold");
+    e.target.className = "bold";
+  }
+});
 
 let ham_btn = document.querySelector(".fa-bars-staggered");
 let ham_active = document.querySelector(".hamburger");
-ham_btn.addEventListener("click",()=>{
-ham_active.classList.toggle("hamburger_active");
-})
-
-
-  
+ham_btn.addEventListener("click", () => {
+  ham_active.classList.toggle("hamburger_active");
+});
